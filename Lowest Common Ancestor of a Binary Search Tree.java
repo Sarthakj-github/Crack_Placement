9fba563edbb73e760114a9475d7654a08d0b5b83
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2024-07-31 18:19:51 +0530

    Update Lowest Common Ancestor of a Binary Search Tree.java

--- Lowest Common Ancestor of a Binary Search Tree.java
+++ Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,17 +1,14 @@
 class Solution {
-    TreeNode ans,P,Q;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ans=null;   P=p;    Q=q;
-        trav(root);
-        return ans;
-    }
-    int trav(TreeNode root){
-        if(root!=null){
-            int a = (root==P || root==Q)?1:0;
-            a+=trav(root.left)+trav(root.right);
-            if(ans==null && a==2)   ans=root;
-            return a;
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
         }
-        return 0;
+        return null;
     }
 }
