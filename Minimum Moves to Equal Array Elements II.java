42107fa64c20f4105c4c1c45ff19a12e81404109
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2024-08-04 21:52:27 +0530

    Create Minimum Moves to Equal Array Elements II.java

---
+++ Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int median = nums[n / 2];
+        int ans = 0;
+        for (int num : nums) {
+            ans += Math.abs(num - median);
+        }
+        return ans;
+    }
+}
