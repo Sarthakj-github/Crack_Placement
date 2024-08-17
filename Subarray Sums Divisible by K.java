8a7bf2e0c7f2a63c4e413b3e0999bfdd60f390d3
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2024-08-18 00:54:14 +0530

    Update Subarray Sums Divisible by K.java

--- Subarray Sums Divisible by K.java
+++ Subarray Sums Divisible by K.java
@@ -1,22 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        nums[0]%=k;
-        for(int i=1;i<nums.length;i++){
-            nums[i] = (nums[i]+nums[i-1])%k;
+        Map<Integer, Integer> d = new HashMap<>();
+        int ans = 0, prefixSum = 0;
+
+        d.put(0, 1); // Account for the case when the prefix sum itself is divisible by k
+
+        for (int num : nums) {
+            prefixSum = (prefixSum + num) % k;
+            if (prefixSum < 0) prefixSum += k; // Handle negative remainders
+            
+            ans += d.getOrDefault(prefixSum, 0);
+            d.put(prefixSum, d.getOrDefault(prefixSum, 0) + 1);
         }
 
-        Map<Integer,Integer> d = new HashMap<>();
-        int ans=0,p;
-        for(int i : nums){
-            if(i==0)  d.put(i,d.getOrDefault(i,0)+1);
-            else{
-                if(i>0) p = i-k;
-                else    p = k+i;
-                if(d.containsKey(p))    ans+=d.get(p);
-            }
-            if(d.containsKey(i)) ans+=d.get(i);
-            if(i!=0)    d.put(i,d.getOrDefault(i,0)+1);
-        }
         return ans;
     }
 }
